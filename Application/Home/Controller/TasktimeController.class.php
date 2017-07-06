529c435e3764f1434993e789ca554e6efde85750
root <ztwrite@163.com>
2017-07-06 13:14:58 +0800

    升级至1.5.1

---
+++ Application/Home/Controller/TasktimeController.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangtong02
+ * Date: 2017/6/29
+ * Time: 14:07
+ */
+
+namespace Home\Controller;
+
+
+use Think\Controller;
+
+class TasktimeController extends Controller
+{
+    public function tasktime_link()
+    {
+        $this->display();
+    }
+
+}
+
